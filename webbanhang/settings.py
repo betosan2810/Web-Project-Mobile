29dbacee192c621abb21373f11a13b6140ed0fa7
Le Viet <vle80444@gmail.com>
2023-08-29 23:13:19 +0700

    sửa ảnh

--- webbanhang/settings.py
+++ webbanhang/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'webbanhang.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'web_database',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
